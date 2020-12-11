de0d74880440bb9f1215df24e293f3fbfbde89a5
nathanjh-28 <nathan@nathanjharris.com>
2020-12-10 21:43:54 -0700

    started forms with login and login route

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,5 +1,9 @@
 from flask import render_template
 from myapp import app
+from myapp.forms import LoginForm
+
+
+# Home page maybe with some instructions / login
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +20,29 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
+
+
+# contacts index page
+
+
+
+# add contact routes
+
+# edit contact routes
+
+# delete contact
+
+# contact show page / add comment
+
+# edit comment
+
+# delete comment
+
+
+
+
